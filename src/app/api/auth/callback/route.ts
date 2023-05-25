23859296f0f1cb4edea2de0ad793199eb8c1e870
Alexandre Nascimento <alexandre.sfi@hotmail.com>
2023-05-24 22:44:10 -0300

    feat:create  middleware and cookies redirectTo

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -5,13 +5,16 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
 
+  // vem do midlleware
+  const redirectTo = request.cookies.get('redirectTo')?.value
+
   const registerResponse = await api.post('/register', {
     code,
   })
 
   const { token } = registerResponse.data
 
-  const redirect = new URL('/', request.url)
+  const redirect = redirectTo ?? new URL('/', request.url)
 
   const cookieExpiresInSeconds = 60 * 60 * 24 * 30
 
